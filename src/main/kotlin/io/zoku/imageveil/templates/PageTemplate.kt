7581e4a6913ca267b9be605de1577cef932dfa45
zoku <zoku2020@gmail.com>
2018-12-18 15:45:40 +0100

    chore(scripts): Use one single download for libs

--- src/main/kotlin/io/zoku/imageveil/templates/PageTemplate.kt
+++ src/main/kotlin/io/zoku/imageveil/templates/PageTemplate.kt
@@ -68,10 +68,7 @@ object PageTemplate {
                     }
                 }
 
-                script(type = "text/javascript", src = "${request.contextPath}/assets/js/lib/jquery-3.3.1.min.js") {}
-                script(type = "text/javascript", src = "${request.contextPath}/assets/js/lib/jquery-ui.min.js") {}
-                script(type = "text/javascript", src = "${request.contextPath}/assets/js/lib/jquery-ui.touch-punch.min.js") {}
-
+                script(type = "text/javascript", src = "${request.contextPath}/assets/js/lib/jquery-all.min.js") {}
                 script(type = "text/javascript", src = "${request.contextPath}/assets/js/application.js") {}
             }
         }
