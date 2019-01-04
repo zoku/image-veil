b3109d544360478f074a84ba5706a217ecae7184
zoku <zoku2020@gmail.com>
2019-01-04 16:39:30 +0100

    feat(statistics): Beautiful with different modes

--- src/main/resources/javascript/statistics.js
+++ src/main/resources/javascript/statistics.js
@@ -7,10 +7,15 @@
             labels: dates,
             datasets: [{
                 label: 'Uses',
-                data: numbers
+                data: numbers,
+                borderColor: '#0d656d',
+                backgroundColor: 'rgba(13, 101, 109, 0.1)'
             }]
         },
         options: {
+            legend: {
+                display: false
+            },
             scales: {
                 yAxes: [{
                     ticks: {
